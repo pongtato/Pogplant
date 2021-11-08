4e6af1aec73e24ab40e660e3c5ba5c2516d9b3fb
Oon Vinceen <callmegoosey@gmail.com>
2021-11-08 12:17:28 +0800

    Created base reflection for particle system

--- Pogplant/Pogplant Driver/Src/ECS/Components/Reflection_for_components.h
+++ Pogplant/Pogplant Driver/Src/ECS/Components/Reflection_for_components.h
@@ -61,6 +61,32 @@ namespace Components
 			.property("m_Near", &Camera::m_Near)
 			.property("m_Far", &Camera::m_Far)
 			.property("m_Zoom", &Camera::m_Zoom);
+
+		rttr::registration::class_<ParticleSystem::CurveVariable>("CurveVariable")
+			.property("m_CurvePoints", &ParticleSystem::CurveVariable::m_CurvePoints)
+			.property("m_MultiplierMin", &ParticleSystem::CurveVariable::m_MultiplierMin)
+			.property("m_MultiplierMax", &ParticleSystem::CurveVariable::m_MultiplierMax)
+			.property("m_CurveMin", &ParticleSystem::CurveVariable::m_CurveMin)
+			.property("m_CurveMax", &ParticleSystem::CurveVariable::m_CurveMax);
+
+		rttr::registration::class_<ParticleSystem>("ParticleSystem")
+			.property("m_Color", &ParticleSystem::m_Color)
+			.property("m_SpawnDirection", &ParticleSystem::m_SpawnDirection)
+			.property("m_Force", &ParticleSystem::m_Force)
+			.property("m_Speed", &ParticleSystem::m_Speed)
+			.property("m_Scale", &ParticleSystem::m_Scale)
+			.property("m_SpawnRadius", &ParticleSystem::m_SpawnRadius)
+			.property("m_Delay", &ParticleSystem::m_Delay)
+			//.property("m_TexID", &ParticleSystem::m_TexID)
+			.property("m_SpawnCount", &ParticleSystem::m_SpawnCount)
+			.property("m_Loop", &ParticleSystem::m_Loop)
+			.property("m_Burst", &ParticleSystem::m_Burst)
+			.property("m_RandomRotate", &ParticleSystem::m_RandomRotate)
+			.property("m_Play", &ParticleSystem::m_Play);
+
+		rttr::registration::class_<Canvas>("Canvas")
+			.property("m_Color", &Canvas::m_Color);
+			//.property("m_TexID", &Canvas::m_TexID);
 	}
 }
 
