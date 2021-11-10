3bca6d7692868dcf6aef67531dc9c20c3cab496b
Ng Tian Kiat <ngtiankiat97@gmail.com>
2021-11-10 17:05:16 +0800

    Initialized followspline object to starting waypoint transform, and reduced jerkiness.

--- Pogplant/Scripting/Scripts/FollowSpline.cs
+++ Pogplant/Scripting/Scripts/FollowSpline.cs
@@ -40,6 +40,7 @@ namespace Scripting
 
         private bool lockRotation = false;
 
+        private bool isInit = false;
         public override void Init(ref uint _entityID)
         {
             entityID = _entityID;
@@ -74,18 +75,14 @@ namespace Scripting
         // Update is called once per frame
         public override void Update(ref Transform transform, ref Rigidbody rigidbody, ref float dt)
         {
+            if (!isInit)
+            {
+                isInit = true;
+                transform.Position = waypoints[0].Position;
+                transform.Rotation = waypoints[0].Rotation;
+            }
+
             FollowWaypoints(ref transform, ref dt);
-
-            
-            //catmullRom.WriteControlPointsToFile();
-
-            //temp debug
-            //for (int i = 0; i < waypoints.Length; ++i)
-            //    DebugDraw.DebugSphere(waypoints[i].X, waypoints[i].Y, waypoints[i].Z, 0.0f, 0.0f, 0.0f, 0.2f);
-
-            //for (int i = 0; i < waypoints.Length; ++i)
-            //    DebugDraw.DebugSphere(waypoints[i].Position, Vector3.Zero(), 0.2f, 36);
-
         }
 
         public override void LateUpdate(ref Transform transform, ref Rigidbody rigidbody, ref float dt)
@@ -98,18 +95,19 @@ namespace Scripting
             follow_speed = speed;
         }
 
-        private float lerpSpeed = 10.0f;
-
         // This function interpolates the gameobjects transform between the current and next waypoint.
         private void FollowWaypoints(ref Transform transform, ref float dt)
         {
             // Only move if we are not at the end of path.
             if (!isEnd)
             {
+
+                float translation_lerpSpeed = 10.0f;
+
                 UpdateCurrentWaypoint(alpha);
                 time_between_waypoint += dt;
                 alpha = d_alpha * time_between_waypoint; // Calculate the current alpha between the 2 waypoints
-                transform.Position += (Vector3.Lerp(waypoints[current_waypoint_index - 1].Position, waypoints[current_waypoint_index].Position, alpha) - transform.Position) * lerpSpeed * dt;
+                transform.Position += (Vector3.Lerp(waypoints[current_waypoint_index - 1].Position, waypoints[current_waypoint_index].Position, alpha) - transform.Position) * translation_lerpSpeed * dt;
                 //transform.Position = Vector3.MoveTowards(transform.Position, waypoints[current_waypoint_index], follow_speed * dt);
                 //if (transform.Position != waypoints[current_waypoint_index])
                 //    transform.Rotation = Quaternion.Slerp(transform.Rotation, Quaternion.LookRotation(waypoints[current_waypoint_index + 1] - transform.Position), rotation_speed * Time.deltaTime);
@@ -117,10 +115,11 @@ namespace Scripting
                 //if (lockRotation)
                 //{
 
-                transform.Rotation = Vector3.Lerp(transform.Rotation, waypoints[current_waypoint_index + 1].Rotation, lerpSpeed * dt);
+                float rotation_lerp_speed = 0.4f;
+                //transform.Rotation = Vector3.Lerp(transform.Rotation, waypoints[current_waypoint_index + 1].Rotation, lerpSpeed * dt);
                 //transform.Rotation.Y *= -1.0f;
                 //Console.WriteLine("Rotation: " + transform.Rotation.X + ", " + transform.Rotation.Y + ", " + transform.Rotation.Z);
-                //transform.Rotation += (Vector3.RotateTowards(waypoints[current_waypoint_index - 1].Rotation, waypoints[current_waypoint_index].Rotation, alpha) - transform.Rotation) * lerpSpeed * dt;
+                transform.Rotation += (Vector3.RotateTowards(waypoints[current_waypoint_index - 1].Rotation, waypoints[current_waypoint_index].Rotation, alpha) - transform.Rotation) * rotation_lerp_speed * dt;
                 //transform.Rotation = Vector3.RotateTowards(transform.Rotation, waypoints[current_waypoint_index + 1].Rotation, lerpSpeed * dt);
 
                 //Console.WriteLine("Rotation: " + transform.Rotation.X + ", " + transform.Rotation.Y + ", " + transform.Rotation.Z);
