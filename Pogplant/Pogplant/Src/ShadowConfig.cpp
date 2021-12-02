0d595a801f2a60a3ff315e0150be2d5dbc75c438
Aloysius Chun <aloysius_chun@hotmail.com>
2021-12-03 00:13:59 +0800

    Soften shadows + Blending

--- Pogplant/Pogplant/Src/ShadowConfig.cpp
+++ Pogplant/Pogplant/Src/ShadowConfig.cpp
@@ -3,6 +3,6 @@
 namespace Pogplant
 {
 	glm::mat4 ShadowCFG::m_LightProj = glm::mat4{ 1 };
-	int ShadowCFG::m_ShadowMapW = 2048;
-	int ShadowCFG::m_ShadowMapH = 2048;
+	int ShadowCFG::m_ShadowMapW = 8192;
+	int ShadowCFG::m_ShadowMapH = 8192;
 }
