c99ff77f626628a47b609108eb8afe643b206943
Gabriel Wong <auratigital@gmail.com>
2022-01-29 21:54:06 +0800

    L1Boss changes - Added enemy spawn points - Added damage threshold behaviour - Added death on health

--- Pogplant/Scripting/MonoScripts/L1BossBehaviour.cs
+++ Pogplant/Scripting/MonoScripts/L1BossBehaviour.cs
@@ -15,16 +15,31 @@ namespace Scripting
 
 		/**************************
 		*
-		* Core Variables
+		* Variables
 		*
 		**************************/
+		#region[Core Variables]
 		/**> The health of the boss core*/
 		public float m_coreHealth;
 
 		/**> How much damage the boss will take before going into protection mode*/
 		public float m_coreProtectionThreshold;
-		public float m_coreProtectionDuration;
 
+		/**> How much time before the damage threshold gets reset*/
+		public float m_coreProtectionDamageDuration;
+
+
+
+		public uint[] mID_ventSpawnpoints = new uint[6];
+
+		#endregion
+
+		/**************************
+		*
+		* Structs
+		*
+		**************************/
+		#region[Structures]
 		public struct BossStateBehaviour
 		{
 			//Default constructor
@@ -51,16 +66,29 @@ namespace Scripting
 			{
 				m_timer = 0f;
 				m_stateDuration = 1f;
+				m_timeSinceLastDamage = 0f;
+				m_damageTakenPeriod = 0f;
 			}
 
 			public float m_timer;
 			public float m_stateDuration;
+			public float m_timeSinceLastDamage;
+
+			/**> Damage taken in this period of time*/
+			public float m_damageTakenPeriod;
 		}
+		#endregion
 
 		RuntimeStateVariables m_runStateInfo;
 
 		BossStateBehaviour[] m_stateBehaviours = new BossStateBehaviour[(int)L1Boss.BOSS_BEHAVIOUR_STATE.TOTAL];
 
+		/******************************************************************************/
+		/*!
+		\brief
+			Init for entity and singleton
+		*/
+		/******************************************************************************/
 		public override void Init(ref uint _entityID)
 		{
 			entityID = _entityID;
@@ -77,9 +105,16 @@ namespace Scripting
 		{
 			m_coreHealth = ECS.GetValue<float>(entityID, 100f, "CoreHealth");
 			m_coreProtectionThreshold = ECS.GetValue<float>(entityID, 1f, "CoreProtectionThreshold");
-			m_coreProtectionDuration = ECS.GetValue<float>(entityID, 10f, "CoreProtectionDuration");
+			m_coreProtectionDamageDuration = ECS.GetValue<float>(entityID, 1f, "CoreProtectionDamageDuration");
 
 			InitStateBehaviours();
+
+			uint bossPanelSpawns = ECS.FindEntityWithName("BossPanelSpawnPoints");
+
+			for (int i = 0; i < 6; ++i)
+			{
+				mID_ventSpawnpoints[i] = ECS.FindChildEntityWithName(bossPanelSpawns, (i + 1).ToString());
+			}
 		}
 
 		void InitStateBehaviours()
@@ -94,6 +129,11 @@ namespace Scripting
 			m_stateBehaviours[(int)L1Boss.BOSS_BEHAVIOUR_STATE.PROTECTION].shouldReturnToDefault = true;
 			m_stateBehaviours[(int)L1Boss.BOSS_BEHAVIOUR_STATE.PROTECTION].stateDurationMin = 5f;
 			m_stateBehaviours[(int)L1Boss.BOSS_BEHAVIOUR_STATE.PROTECTION].stateDurationMax = 10f;
+
+			m_stateBehaviours[(int)L1Boss.BOSS_BEHAVIOUR_STATE.LAUNCH_NORMAL_ADDS].isVulnerable = false;
+			m_stateBehaviours[(int)L1Boss.BOSS_BEHAVIOUR_STATE.LAUNCH_NORMAL_ADDS].shouldReturnToDefault = false;
+			m_stateBehaviours[(int)L1Boss.BOSS_BEHAVIOUR_STATE.LAUNCH_NORMAL_ADDS].stateDurationMin = 5f;
+			m_stateBehaviours[(int)L1Boss.BOSS_BEHAVIOUR_STATE.LAUNCH_NORMAL_ADDS].stateDurationMax = 10f;
 
 			m_stateBehaviours[(int)L1Boss.BOSS_BEHAVIOUR_STATE.DEATH_SEQUENCE].isVulnerable = false;
 			m_stateBehaviours[(int)L1Boss.BOSS_BEHAVIOUR_STATE.DEATH_SEQUENCE].shouldReturnToDefault = false;
@@ -134,7 +174,7 @@ namespace Scripting
 
 			//Other code logic
 
-			/*switch (L1Boss.m_singleton.current_state)
+			switch (L1Boss.m_singleton.current_state)
 			{
 				case L1Boss.BOSS_BEHAVIOUR_STATE.MOVING:
 					break;
@@ -142,17 +182,30 @@ namespace Scripting
 					break;
 				case L1Boss.BOSS_BEHAVIOUR_STATE.DEATH_SEQUENCE:
 					break;
-			}//*/
+			}
+
+			if (m_runStateInfo.m_damageTakenPeriod > m_coreProtectionThreshold)
+			{
+				TriggerNextState(L1Boss.BOSS_BEHAVIOUR_STATE.PROTECTION);
+				m_runStateInfo.m_damageTakenPeriod = 0f;
+				Console.WriteLine("Boss hit damage threshold, entering protection");
+			}
 
 			m_runStateInfo.m_timer += dt;
+			m_runStateInfo.m_timeSinceLastDamage += dt;
 
-			if(m_runStateInfo.m_timer > m_runStateInfo.m_stateDuration)
+			if (m_runStateInfo.m_timer > m_runStateInfo.m_stateDuration)
 			{
 				if(m_stateBehaviours[(int)L1Boss.m_singleton.current_state].shouldReturnToDefault)
 				{
 					TriggerNextState(L1Boss.BOSS_BEHAVIOUR_STATE.MOVING);
-					Console.WriteLine("Changing to default");
+					Console.WriteLine("Changing to MOVING");
 				}
+			}
+
+			if(m_runStateInfo.m_timeSinceLastDamage > m_coreProtectionDamageDuration)
+			{
+				m_runStateInfo.m_damageTakenPeriod = 0f;
 			}
 		}
 		public override void LateUpdate(float dt)
@@ -174,14 +227,14 @@ namespace Scripting
 			//Invulnerable in protection mode
 			if(!m_stateBehaviours[(int)L1Boss.m_singleton.current_state].isVulnerable)
 			{
-				Console.WriteLine("Boss Invulnerable");
 				return;
 			}
-
 
 			Console.WriteLine("Boss Taken damage");
 
 			m_coreHealth -= 1f;
+			m_runStateInfo.m_damageTakenPeriod += 1f;
+			m_runStateInfo.m_timeSinceLastDamage = 0f;
 
 			if (m_coreHealth <= 0f)
 			{
