99f0ffd7c8e63ac81663af904bb330a6443454d0
Gabriel Wong <auratigital@gmail.com>
2022-02-09 23:28:01 +0800

    - Added solution cleaner

---
+++ Pogplant/cleansolution.bat
@@ -0,0 +1,12 @@
+rmdir /Q /s bin
+rmdir /Q /s bin-int
+rmdir /Q /s .vs
+rmdir /Q /s "Pogplant Driver\Mono"
+rmdir /Q /s "Pogplant Driver\Resources\KekFiles"
+rmdir /Q /s "Scripting\obj"
+rmdir /Q /s "Scripting\bin"
+del /f "Pogplant Driver\assimp-vc142-mt.dll"
+del /f "Pogplant Driver\assimp-vc142-mtd.dll"
+del /f "Pogplant Driver\glew32.dll"
+del /f "Pogplant Driver\imgui.ini"
+del /f "Pogplant Driver\Pogplant Compiler.exe"
